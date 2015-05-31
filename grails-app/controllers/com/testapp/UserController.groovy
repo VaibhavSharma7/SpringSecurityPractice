eeab7cecde889cd4992c16dbeaebb92be27d23e3
Vaibhav Sharma <osvaibhav7@gmail.com>
2015-05-31 16:05:20 +0530

    first commit

---
+++ grails-app/controllers/com/testapp/UserController.groovy
@@ -0,0 +1,109 @@
+package com.testapp
+
+import grails.plugin.springsecurity.annotation.Secured
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+@Secured('permitAll')
+
+@Transactional(readOnly = true)
+class UserController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond User.list(params), model:[userInstanceCount: User.count()]
+    }
+
+    def show(User userInstance) {
+        respond userInstance
+    }
+
+    def create() {
+        respond new User(params)
+    }
+
+    @Transactional
+    def save(User userInstance) {
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userInstance.hasErrors()) {
+            respond userInstance.errors, view:'create'
+            return
+        }
+        println("userinstance :: "+userInstance.dump())
+        userInstance=userInstance.save flush:true
+        Role role=Role.findByAuthority("ROLE_ADMIN")
+        UserRole.create userInstance, role, true
+//        UserRole userRole=new UserRole(user: userInstance,role:new Role(authority: 'ROLE_ADMIN').save(flush: true))
+//        userRole.save(flush: true)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])
+                redirect userInstance
+            }
+            '*' { respond userInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(User userInstance) {
+        respond userInstance
+    }
+
+    @Transactional
+    def update(User userInstance) {
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userInstance.hasErrors()) {
+            respond userInstance.errors, view:'edit'
+            return
+        }
+
+        userInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
+                redirect userInstance
+            }
+            '*'{ respond userInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(User userInstance) {
+
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        userInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
